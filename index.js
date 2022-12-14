6eb5b95640d42ae405c6acd6ccff2226cea88186
topher ludlow <topherludlow@protonmail.com>
2022-12-14 10:21:23 -0800

    commit tree 0d6c86fef12493ff05c802ceff2741b55d86635c

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+
 function addingEventListener() {
+const input = document.getElementById('button');
+    alert('I was clicked!');
+input.addEventListener('click', addingEventListener);
 }
+addingEventListener()
+
+/*
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
+*/
